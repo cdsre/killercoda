3d72c13c484e2e78466371846e2b685007647cf8
Chris Doyle <uk.chris.doyle+cdsre@gmail.com>
2024-11-17 23:22:05 +0000

    Running docker ctk image in the foreground

--- chaostoolkit/000-running-ctk/foreground.sh
+++ chaostoolkit/000-running-ctk/foreground.sh
@@ -1,7 +1,2 @@
 echo -n "Installing scenario..."
-while [ ! -f /tmp/finished ];
-do
-  echo -n "."
-  sleep 1;
-done
-echo DONE
+docker run -it -v $(pwd):/ctk -w /ctk chaostoolkit/chaostoolkit@sha256:4a82ca6886674d617573b10fd8ceda9e6152fd2696ca38181b6487cb2cf44d62
--- chaostoolkit/000-running-ctk/setup.sh
+++ chaostoolkit/000-running-ctk/setup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -it -v $(pwd):/ctk -w /ctk chaostoolkit/chaostoolkit@sha256:4a82ca6886674d617573b10fd8ceda9e6152fd2696ca38181b6487cb2cf44d62
+/bin/true
