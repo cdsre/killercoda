146d1626e0658b6c56d8f377101065287f927e0c
Chris Doyle <uk.chris.doyle+cdsre@gmail.com>
2024-11-18 20:42:01 +0000

    Updating the step1 verify and background tasks

--- chaostoolkit/000-running-ctk/step1/background.sh
+++ chaostoolkit/000-running-ctk/step1/background.sh
@@ -2,7 +2,7 @@
 
 cd /tmp || exit
 git clone https://github.com/chaostoolkit/chaostoolkit-documentation-code
-cp chaostoolkit-documentation-code/tutorials/a-simple-walkthrough ~/ctk-simple
+cp -r chaostoolkit-documentation-code/tutorials/a-simple-walkthrough ~/ctk-simple
 
 cd ~/ctk-simple || exit
 pip install -U -r requirements.txt
