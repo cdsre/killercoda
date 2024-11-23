ddb1986ba816d98bcef5c477c718e395c634ade4
Chris Doyle <uk.chris.doyle+cdsre@gmail.com>
2024-11-23 00:40:49 +0000

    Updating hostname for the assets

--- chaostoolkit/000-running-ctk/init/background.sh
+++ chaostoolkit/000-running-ctk/init/background.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # will run in background. can setup various things for the scenario for user not to see
 # DO NOT change file: add changes in init.sh
 
-FILE=/tmp/init.sh; while ! test -f ${FILE}; do clear; sleep 0.1; done; bash ${FILE} > /tmp/init.log
+FILE=/tmp/init.sh
+while ! test -f ${FILE}
+do
+  clear
+  sleep 0.1
+done;
+
+bash ${FILE} > /tmp/init.log
--- chaostoolkit/000-running-ctk/init/foreground.sh
+++ chaostoolkit/000-running-ctk/init/foreground.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-FILE=/tmp/wait-init.sh; while ! test -f ${FILE}; do clear; sleep 0.1; done; bash ${FILE}
+FILE=/tmp/wait-init.sh
+while ! test -f ${FILE}
+do
+  clear
+  sleep 0.1
+done
+
+bash ${FILE}
